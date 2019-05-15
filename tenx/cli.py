ef156a290a8274c6e5f8282dc51a5f67ae770dda
Eddie Belter <ebelter@gmail.com>
2019-05-15 19:31:09 +0000

    TenX CLI
    rename command variable names

--- tenx/cli.py
+++ tenx/cli.py
@@ -1,7 +1,7 @@
 import click, os
 
 from tenx.version import __version__
-from tenx import app, reads, report, util
+from tenx import app, assembly, reads, report, util
 
 CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
 @click.group(context_settings=CONTEXT_SETTINGS)
@@ -10,7 +10,7 @@ def cli():
     """
     10X CLI
     """
-    app.TenxApp(os.environ.get('TENX_CONFIG', None))
+    app.TenxApp(os.environ.get('TENX_CONFIG_FILE', None))
     pass
 
 # ASSEMBLY
@@ -21,27 +21,27 @@ def cli():
 # FUTURE download, list, view
 
 @click.group()
-def assembly():
+def tenx_assembly_cmd():
     """
     Commands, Pipeline, and Helpers for Assemblies
     """
     pass
 
-cli.add_command(assembly)
+cli.add_command(tenx_assembly_cmd, name="assembly")
 
 @click.command(short_help="create an assembly with supernova")
 @click.argument('sample-name', type=click.STRING)
 def asm_assemble(sample_name):
     assert bool(app.TenxApp.config) is True, "Must provide tenx yaml config file!"
     assembly.run_assemble(assembly.TenxAssembly(sample_name=sample_name))
-assembly.add_command(asm_assemble, name="assemble")
+tenx_assembly_cmd.add_command(asm_assemble, name="assemble")
 
 @click.command(short_help="run mkoutput on an assembly")
 @click.argument('sample-name', type=click.STRING)
 def asm_mkoutput(sample_name):
     assert bool(app.TenxApp.config) is True, "Must provide tenx yaml config file!"
     assembly.run_mkoutput(assembly.TenxAssembly(sample_name=sample_name))
-assembly.add_command(asm_mkoutput, name="mkoutput")
+tenx_assembly_cmd.add_command(asm_mkoutput, name="mkoutput")
 
 @click.command(short_help="run the full supernova assembly pipeline")
 @click.argument('sample-name', type=click.STRING)
@@ -53,33 +53,33 @@ def asm_pipeline(sample_name):
     assembly.run_mkoutput(asm)
     print( report.run_duration_basic(util.run_duration(asm.directory)) )
     assembly.run_upload(asm)
-assembly.add_command(asm_pipeline, name="pipeline")
+tenx_assembly_cmd.add_command(asm_pipeline, name="pipeline")
 
 @click.command(short_help="Send the assembly to the cloud")
 @click.argument('sample-name', type=click.STRING)
 def asm_upload(sample_name):
     assert bool(app.TenxApp.config) is True, "Must provide tenx yaml config file!"
     assembly.run_mkoutput(assembly.TenxAssembly(sample_name=sample_name))
-assembly.add_command(asm_upload, name="upload")
+tenx_assembly_cmd.add_command(asm_upload, name="upload")
 #-- ASSEMBLY
 
 # READS
 # - download (fetch reads from the cloud)
 @click.group(name="reads")
-def reads_cli():
+def tenx_reads_cmd():
     """
     Commands and Helpers for 10X Reads
     """
     pass
 
-cli.add_command(reads_cli)
+cli.add_command(tenx_reads_cmd)
 
 @click.command(short_help="Download reads from the cloud!")
 @click.argument('sample-name', type=click.STRING)
 def reads_download(sample_name):
     assert bool(app.TenxApp.config) is True, "Must provide tenx yaml config file!"
     reads.download(reads.TenxReads(sample_name=sample_name))
-reads_cli.add_command(reads_download, name="download")
+tenx_reads_cmd.add_command(reads_download, name="download")
 #-- READS
 
 # UTIL
