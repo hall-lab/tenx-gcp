70bed63420ca51bc1dcdda5cb0b90eb89ac2ee47
Eddie Belter <ebelter@gmail.com>
2019-09-03 16:26:20 +0000

    Begin and end motd scripts

---
+++ resources/scripts/longranger/end_msg.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+import os, shutil
+
+def end_motd():
+    if os.path.exists("/etc/motd.bak"):
+        shutil.copy("/etc/motd.bak", "/etc/motd") 
+    else
+        f = open('/etc/motd', 'w')
+        f.write('')
+        f.close()
+
+    msg = """
+*** LONGRANGER AND TENX INSTALLATION COMPLETE ***
+***        YOU MAY NEED TO LOG OUT/IN        ***
+"""
+    subprocess.call(['wall', '-n', msg])
+
+#-- end_motd
+
+if __name__ == '__main__':
+    end_motd()
