1e0198b042b4de527c784c9600f116e458603154
Eddie Belter <ebelter@gmail.com>
2019-10-01 17:33:43 -0500

    Remove older slurm aln scripts

--- slurm-scripts/alignment-pipeline.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#
-#SBATCH --job-name={TENX_SAMPLE}_ALN_PIPE
-#SBATCH --output=%j.out
-
-/usr/bin/env tenx alignment pipeline {TENX_SAMPLE} {TENX_REF}
--- slurm-scripts/alignment-upload.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#
-#SBATCH --job-name={TENX_SAMPLE}_UL_ALN
-#SBATCH --output=%j.out
-
-/usr/bin/env tenx alignment upload {TENX_SAMPLE}
