39886237de4c16cb0f20b6377380711a6cb13479
Eddie Belter <ebelter@gmail.com>
2020-03-18 21:32:02 -0500

    ASM CLI tests use CLiRunner

--- tests/test_asm_cli.py
+++ tests/test_asm_cli.py
@@ -1,41 +1,49 @@
 import subprocess, unittest
+from click.testing import CliRunner
+
+from tenx.asm_cli import tenx_asm_cli,  asm_assemble,  asm_mkoutput,  asm_pipeline,  asm_rm_asm_files_cmd,  asm_upload
 
 class TenxAsmCliTest(unittest.TestCase):
 
-    # ASSEMBLY
     def test1_tenx_assembly(self):
-        rv = subprocess.call(['tenx', 'asm'])
-        self.assertEqual(rv, 0)
+        runner = CliRunner()
+        result = runner.invoke(tenx_asm_cli, [])
+        self.assertEqual(result.exit_code, 0)
 
     def test2_tenx_assembly_assemble(self):
-        rv = subprocess.call(['tenx', 'asm', 'assemble', '--help'])
-        self.assertEqual(rv, 0)
-        rv = subprocess.call(['tenx', 'asm', 'assemble'])
-        self.assertEqual(rv, 2)
-
-    def test2_tenx_assembly_download(self):
-        rv = subprocess.call(['tenx', 'asm', 'download', '--help'])
-        self.assertEqual(rv, 0)
-        rv = subprocess.call(['tenx', 'asm', 'download'])
-        self.assertEqual(rv, 2)
+        runner = CliRunner()
+        result = runner.invoke(asm_assemble, ["--help"])
+        self.assertEqual(result.exit_code, 0)
+        result = runner.invoke(asm_assemble, [])
+        self.assertEqual(result.exit_code, 2)
 
     def test2_tenx_assembly_pipeline(self):
-        rv = subprocess.call(['tenx', 'asm', 'pipeline', '--help'])
-        self.assertEqual(rv, 0)
-        rv = subprocess.call(['tenx', 'asm', 'pipeline'])
-        self.assertEqual(rv, 2)
+        runner = CliRunner()
+        result = runner.invoke(asm_pipeline, ["--help"])
+        self.assertEqual(result.exit_code, 0)
+        result = runner.invoke(asm_pipeline, [])
+        self.assertEqual(result.exit_code, 2)
 
     def test2_tenx_assembly_mkoutput(self):
-        rv = subprocess.call(['tenx', 'asm', 'mkoutput', '--help'])
-        self.assertEqual(rv, 0)
-        rv = subprocess.call(['tenx', 'asm', 'mkoutput'])
-        self.assertEqual(rv, 2)
+        runner = CliRunner()
+        result = runner.invoke(asm_mkoutput, ["--help"])
+        self.assertEqual(result.exit_code, 0)
+        result = runner.invoke(asm_mkoutput, [])
+        self.assertEqual(result.exit_code, 2)
+
+    def test2_tenx_assembly_rm_asm_files_cmd(self):
+        runner = CliRunner()
+        result = runner.invoke(asm_rm_asm_files_cmd, ["--help"])
+        self.assertEqual(result.exit_code, 0)
+        result = runner.invoke(asm_rm_asm_files_cmd, [])
+        self.assertEqual(result.exit_code, 2)
 
     def test2_tenx_assembly_upload(self):
-        rv = subprocess.call(['tenx', 'asm', 'upload', '--help'])
-        self.assertEqual(rv, 0)
-        rv = subprocess.call(['tenx', 'asm', 'upload'])
-        self.assertEqual(rv, 2)
+        runner = CliRunner()
+        result = runner.invoke(asm_upload, ["--help"])
+        self.assertEqual(result.exit_code, 0)
+        result = runner.invoke(asm_upload, [])
+        self.assertEqual(result.exit_code, 2)
 
 # -- TenxAsmCliTest
 
