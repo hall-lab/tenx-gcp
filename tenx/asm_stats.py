674fb6d67c0a29742a84a23903031b7a44326b53
Eddie Belter <ebelter@gmail.com>
2020-04-23 14:31:22 -0500

    Assembly stats command from refimp

--- tenx/asm_stats.py
+++ tenx/asm_stats.py
@@ -1,26 +1,39 @@
-import click, os, re
+import click, jinja2, json, os, re, yaml
 from Bio import SeqIO
 
 @click.command(help="generate assembly stats")
-@click.option("--fasta-fn", type=click.STRING, help="Fasta file name.")
-@click.option("--min-gap-size", type=click.STRING, help="Minimum gap size")
-@click.option("--output-fn", type=click.STRING, help="Output file name")
-def asm_stats_cmd(fasta_fn, min_gap_size, outout_fn):
+@click.option("--fasta-fn", "-i", type=click.STRING, required=True, help="Fasta file name")
+@click.option("--gap-ns", "-n", type=click.INT, help="Minimum number of Ns for scaffolded assembly gaps")
+@click.option("--output-fn", "-o", type=click.STRING, required=True, help="Output file name")
+@click.option("--output-format", "-f", type=click.Choice(["quick", "yaml", "json"]), default="quick", help="Output format")
+def asm_stats_cmd(fasta_fn, gap_ns, output_fn, output_format):
     """
     Generate Assembly Stats
     """
     if not os.path.exists(fasta_fn):
         raise Exception("Fasta file does not exist: {}".format(fasta_fn))
 
-    if min_gap_size is not None:
+    stats = {}
+    if gap_ns is None:
         contigs = get_contig_lengths(fasta_fn)
-        scaffolds = contigs
+        stats["contigs"] = get_stats(contigs)
     else:
-        scaffolds, contigs = get_scaffold_and_contig_lengths(fasta_fn)
+        scaffolds, contigs = get_scaffold_and_contig_lengths(fasta_fn, gap_ns)
+        stats["scaffolds"] = get_stats(scaffolds)
+        stats["contigs"] = get_stats(contigs)
 
-    scaffold_stats = get_stats(scaffolds)
-    contig_stats = get_stats(contigs)
-    #with open (output_fn, "w") as f:
+    if output_format == "json":
+        output = json.dumps(stats)
+    elif output_format == "yaml":
+        output = yaml.dump(stats)
+    else: # quick
+        template_fn = os.path.join(os.path.dirname(__file__), "templates", "stats.quick.jinja")
+        with open(template_fn, 'r') as f:
+            template = jinja2.Template(f.read())
+        output = template.render(stats=stats, length_buckets=length_buckets())+"\n"
+
+    with open (output_fn, "w") as f:
+        f.write(output)
 
 #-- asm_stats_cmd
 
@@ -33,14 +46,20 @@ def get_contig_lengths(fasta_fn):
 
 #-- get_contig_lengths
 
-def get_scaffold_and_contig_lengths(fasta_fn):
+def get_scaffold_and_contig_lengths(fasta_fn, ns):
     scaffolds = []
     contigs = []
-    scaffold_splitter = re.compile(r"[ATGCatgc]+")
+    gap_splitter = re.compile(r"N{{{},}}".format(ns))
     for seq in SeqIO.parse(fasta_fn, "fasta"):
         scaffold_l = 0
-        for m in re.finditer(scaffold_splitter, str(seq.seq)):
-            contig_l = len(m.group(0))
+        prev_start = 0
+        for gap in re.finditer(gap_splitter, str(seq.seq)):
+            contig_l = gap.span()[0] - prev_start
+            prev_start = gap.span()[1]
+            scaffold_l += contig_l
+            contigs.append(contig_l)
+        if scaffold_l == 0 or prev_start != len(seq):
+            contig_l = len(seq) - prev_start
             scaffold_l += contig_l
             contigs.append(contig_l)
         scaffolds.append(scaffold_l)
@@ -56,15 +75,16 @@ def length_buckets():
 #-- length_buckets
 
 def get_stats(lengths):
-    # TODO
-    # n50
-    # max length id ?
+    length = sum(lengths)
+    count = len(lengths)
     stats = {
-        "total": sum(lengths),
-        "count": len(lengths),
+        "total": length,
+        "count": count,
+        "mean": int(length/count),
+        "max": lengths[-1],
+        "genome_n50": int(length/2),
         "n50_length": 0,
     }
-    stats["genome_n50"] = int(stats["total"]/2)
 
     buckets = length_buckets()
     for b in buckets:
--- tests/test_asm_stats.py
+++ tests/test_asm_stats.py
@@ -1,4 +1,4 @@
-import os, unittest
+import filecmp, os, tempfile, unittest
 from click.testing import CliRunner
 
 from tenx.asm_stats import asm_stats_cmd, get_contig_lengths, get_scaffold_and_contig_lengths, get_stats, length_buckets
@@ -8,26 +8,35 @@ class AsmStatsTest(unittest.TestCase):
         self.data_dn = os.path.join(os.path.dirname(__file__), "data", "asm-stats")
         self.fasta1_fn = os.path.join(self.data_dn, "asm.fasta")
         self.fasta2_fn = os.path.join(self.data_dn, "asm.scaffolded.fasta")
+        self.fasta2_stats_fn = os.path.join(self.data_dn, "asm.scaffolded.fasta.stats")
         self.expected_scaffolds = [ 17004, 350002, 1000001]
         self.expected_contigs = [1, 2001, 5001, 10001, 100001, 250001, 1000001]
+
+        self.temp_d = tempfile.TemporaryDirectory()
+
+    def tearDown(self):
+        self.temp_d.cleanup()
 
     def test1_get_contig_lengths(self):
         contigs = get_contig_lengths(self.fasta1_fn)
         self.assertEqual(contigs, self.expected_contigs)
 
     def test1_get_scaffold_and_contig_lengths(self):
-        scaffolds, contigs = get_scaffold_and_contig_lengths(self.fasta2_fn)
+        scaffolds, contigs = get_scaffold_and_contig_lengths(self.fasta2_fn, 2)
         self.assertEqual(scaffolds, self.expected_scaffolds)
         self.assertEqual(contigs, self.expected_contigs)
 
     def test2_get_stats(self):
+        total = sum(self.expected_scaffolds)
+        count = len(self.expected_scaffolds)
         expected_stats = {
-            "total": sum(self.expected_scaffolds),
-            "count": len(self.expected_scaffolds),
+            "total": total,
+            "count": count,
+            "mean": int(total/count),
+            "max": self.expected_scaffolds[-1],
+            "genome_n50": int(total/2),
+            "n50_length": total,
         }
-        expected_stats["genome_n50"] = int(expected_stats["total"]/2)
-        expected_stats["n50_length"] = expected_stats["total"]
-
         for b in length_buckets():
             expected_stats["_".join([str(b), "count"])] = 0
             expected_stats["_".join([str(b), "length"])] = 0
@@ -40,12 +49,16 @@ class AsmStatsTest(unittest.TestCase):
         stats = get_stats(self.expected_scaffolds)
         self.assertEqual(stats, expected_stats)
 
+        total = sum(self.expected_contigs)
+        count = len(self.expected_contigs)
         expected_stats = {
-            "total": sum(self.expected_contigs),
-            "count": len(self.expected_contigs),
+            "total": total,
+            "count": count,
+            "mean": int(total/count),
+            "max": self.expected_contigs[-1],
+            "genome_n50": int(total/2),
+            "n50_length": total,
         }
-        expected_stats["genome_n50"] = int(expected_stats["total"]/2)
-        expected_stats["n50_length"] = expected_stats["total"]
         for b in length_buckets():
             expected_stats["_".join([str(b), "count"])] = 1
             expected_stats["_".join([str(b), "length"])] = b + 1
@@ -53,7 +66,25 @@ class AsmStatsTest(unittest.TestCase):
         self.assertEqual(stats, expected_stats)
 
     def test4_asm_stats_cmd(self):
-        self.assertEqual(0, 0)
+        runner = CliRunner()
+        result = runner.invoke(asm_stats_cmd, ["--help"])
+        self.assertEqual(result.exit_code, 0)
+        result = runner.invoke(asm_stats_cmd, [])
+        self.assertEqual(result.exit_code, 2)
+
+        stats_fn = os.path.join(self.temp_d.name, "stats.txt")
+        result = runner.invoke(asm_stats_cmd, ["-i", self.fasta2_fn, "-n", 2, "-o", stats_fn, "-f", "quick"])
+        try:
+            self.assertEqual(result.exit_code, 0)
+        except:
+            print(result.output)
+            raise
+        self.assertEqual(filecmp.cmp(stats_fn, self.fasta2_stats_fn), True)
+
+        result = runner.invoke(asm_stats_cmd, ["-i", self.fasta2_fn, "-n", 2, "-o", stats_fn, "-f", "json"])
+        self.assertEqual(result.exit_code, 0)
+        result = runner.invoke(asm_stats_cmd, ["-i", self.fasta2_fn, "-n", 2, "-o", stats_fn, "-f", "yaml"])
+        self.assertEqual(result.exit_code, 0)
 
 # -- AsmStatsTest
 
