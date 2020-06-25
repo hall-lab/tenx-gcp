5e485c09e69f03267010d5cd0f0a0ed618c22916
Eddie Belter <ebelter@gmail.com>
2020-06-24 21:03:28 -0500

    TenxSample

---
+++ tenx/sample.py
@@ -0,0 +1,11 @@
+import os
+
+
+class TenxSample():
+    
+    def __init__(self, base_path, name):
+        self.base_path = base_path
+        self.name = name
+        self.path = os.path.join(self.base_path, name)
+
+#-- TenxSample
