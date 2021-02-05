330a6adeec45d460df2656e219924b5b04f3c832
Eddie Belter <ebelter@gmail.com>
2021-02-04 19:06:33 -0600

    CLI - no more context

--- tenx/cli.py
+++ tenx/cli.py
@@ -6,7 +6,6 @@ from tenx.version import __version__
 CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
 @click.group(context_settings=CONTEXT_SETTINGS)
 @click.version_option(version=__version__)
-@click.pass_context
 def cli():
     """
     10X CLI
